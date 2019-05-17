4c6f58b21335be3f8cde1a563a3ab0f7f20acdd7
Pedro Costa <jules007.cesar@hotmail.com>
2019-05-17 20:39:43 +0200

    Better format the extract file and save dataframe.

--- src/main/scala/DataExtraction.scala
+++ src/main/scala/DataExtraction.scala
@@ -12,33 +12,71 @@ object DataExtraction extends App {
 
   // TODO pedro, john do stats
 
+  def countNull(colName:String) = sum(col(colName).isNull.cast("integer")).alias(colName)
+  def countNotNull(colName:String) = sum(col(colName).isNotNull.cast("integer")).alias(colName)
+
   val spark = SparkSession.builder.config("spark.master", "local[2]").getOrCreate()
 
   import spark.implicits._
 
-  val df = spark.read.option("sep", "\t").option("header", "true").option("nullValue", "\\N").csv("dataset/title.basics.tsv")
-  df.createOrReplaceTempView("titles")
+  val df_basics = spark.read
+                       .option("sep", "\t")
+                       .option("header", "true")
+                       .option("nullValue", "\\N")
+                       .csv("dataset/title.basics.tsv")
 
-  val df_new = df.filter("titleType == \"movie\"")
-  val df3 = df_new.drop("titleType").drop("originalTitle").drop("endYear").na.drop()
+  df_basics.createOrReplaceTempView("titles")
+
+  println("Number of rows of basic.tsv " + df_basics.count())
+
+  val df_basics_filtered = df_basics.filter("titleType == \"movie\"")
+  val df_step0 = df_basics_filtered.drop("titleType")
+                                   .drop("originalTitle")
+                                   .drop("endYear")
 
 
-  // DONE 1 THIBAUT Look for the rating of each movie. Create a new dataframe with the name of the movie and it's rating
+  // DONE 1 THIBAUT Look for the rating of each movie.
+  // Create a new dataframe with the name of the movie and it's rating
 
-  val df_ratings = spark.read.option("sep", "\t").option("header", "true").csv("dataset/title.ratings.tsv")
-  val df_label = df_ratings.drop("numVotes")
+  val df_ratings = spark.read
+                        .option("sep", "\t")
+                        .option("header", "true")
+                        .option("nullValue", "\\N")
+                        .csv("dataset/title.ratings.tsv")
 
-  val df_step1 = df3.join(df_ratings, df_ratings("tconst") === df3("tconst")).drop(df_ratings("tconst")).na.drop()
+  println("Number of rows of ratings.tsv " + df_ratings.count())
+
+  val df_step1 = df_step0.join(df_ratings,
+                               df_ratings("tconst") === df_step0("tconst"))
+                               .drop(df_ratings("tconst"))
+                               .drop("numVotes")
   df_step1.printSchema()
 
-  // DONE 2 JOHN Look for each film's director(s) information is store in title.crew. try to append directos name to dataframe
+  println("Number of rows of join with ratings and basic.tsv " + df_step1.count())
 
 
-  val df_crew = spark.read.option("sep", "\t").option("header", "true").csv("dataset/title.crew.tsv")
+  // DONE 2 JOHN Look for each film's director(s) information is store in title.crew. T
+  // ry to append directos name to dataframe
+
+  val df_crew = spark.read
+                     .option("sep", "\t")
+                     .option("header", "true")
+                     .option("nullValue", "\\N")
+                     .csv("dataset/title.crew.tsv")
+
   df_crew.createOrReplaceTempView("crew")
 
-  // inner join to remove unwanted ids
-  val df_crew_movie = df_step1.join(df_crew, df_crew("tconst") === df_step1("tconst")).drop(df_crew("tconst")).na.drop()
+  println("Number of rows of crew.tsv " + df_crew.count())
+
+  // Inner join to remove unwanted ids
+  val df_crew_movie = df_step1.join(df_crew,
+                                    df_crew("tconst") === df_step1("tconst"))
+                              .drop(df_crew("tconst"))
+                              .na.drop()
+
+  // df_crew_movie.agg(countNull("genres")).show()
+
+  println("Number of rows of join with ratings, crew and basic.tsv " + df_crew_movie.count())
 
   //val nb_directors = df_crew_movie.collect().map(r => (r.get(8).toString.split(",").size))
   //  1 to 10 foreach { i =>
@@ -53,30 +91,31 @@ object DataExtraction extends App {
   //  }
   // 5 is the best value to use
 
-  val parse_directors: Column => Column = x => {
+  val parse_string: Column => Column = x => {
     split(x, ",")
   }
 
-  val df_directors = df_crew_movie.withColumn("directors", parse_directors($"directors")).select(
+  val df_directors = df_crew_movie.withColumn("directors", parse_string($"directors")).select(
     $"tconst",
     $"directors".getItem(0).as("director1"),
     $"directors".getItem(1).as("director2"),
     $"directors".getItem(2).as("director3"),
     $"directors".getItem(3).as("director4"),
-  ).na.fill(Map("director2" -> 0)).na.fill(Map("director3" -> 0)).na.fill(Map("director4" -> 0))
+  ).na.fill(Map("director2" -> 0))
+   .na.fill(Map("director3" -> 0))
+   .na.fill(Map("director4" -> 0))
 
-  val parse_writers: Column => Column = x => {
-    split(x, ",")
-  }
-
-  val df_writers = df_crew_movie.withColumn("writers", parse_directors($"writers")).select(
+  val df_writers = df_crew_movie.withColumn("writers", parse_string($"writers")).select(
     $"tconst",
     $"writers".getItem(0).as("writer1"),
     $"writers".getItem(1).as("writer2"),
     $"writers".getItem(2).as("writer3"),
     $"writers".getItem(3).as("writer4"),
     $"writers".getItem(4).as("writer5"),
-  ).na.fill(Map("writer2" -> 0)).na.fill(Map("writer3" -> 0)).na.fill(Map("writer4" -> 0)).na.fill(Map("writer5" -> 0))
+  ).na.fill(Map("writer2" -> 0))
+   .na.fill(Map("writer3" -> 0))
+   .na.fill(Map("writer4" -> 0))
+   .na.fill(Map("writer5" -> 0))
 
   val df_step2_1 = df_step1.join(df_directors,
     df_directors("tconst") === df_step1("tconst")).drop(df_directors("tconst"))
@@ -89,26 +128,27 @@ object DataExtraction extends App {
 
   //DONE 3 PEDRO split genres into n columns, n beeing the maximum number of genre a movie can have
 
-
-  val parse_genres: Column => Column = x => {
-    split(x, ",")
-  }
-
-  val df_genres = df_crew_movie.withColumn("genres", parse_genres($"genres")).select(
+  val df_genres = df_crew_movie.withColumn("genres", parse_string($"genres")).select(
     $"tconst",
     $"genres".getItem(0).as("genre1"),
     $"genres".getItem(1).as("genre2"),
     $"genres".getItem(2).as("genre3")
-  ).na.fill(Map("genre2" -> 0)).na.fill(Map("genre3" -> 0))
+  ).na.fill(Map("genre2" -> 0))
+   .na.fill(Map("genre3" -> 0))
+
+  /*
+  df_genres.agg(countNotNull("genre1")).show()
+  df_genres.agg(countNotNull("genre2")).show()
+  df_genres.agg(countNotNull("genre3")).show()
+   */
 
   val df_step3 = df_step2.join(df_genres,
     df_genres("tconst") === df_step2("tconst")).drop(df_genres("tconst")).drop(df_step2("genres"))
 
-
   df_step3.printSchema()
 
-
-
   // TODO thibaut export to csv
-  df_step3.write.format("csv").save("dataset/features.csv")
+
+  df_step3.orderBy("startYear").show(50, false)
+  df_step3.write.mode(SaveMode.Overwrite).parquet("features_extract.parquet")
 }
--- src/main/scala/DataPreparation.scala
+++ src/main/scala/DataPreparation.scala
@@ -9,9 +9,15 @@ import org.apache.spark.sql.types._
 
 
 object DataPreparation extends App {
+  val spark = SparkSession.builder.config("spark.master", "local[2]").getOrCreate()
+
+  import spark.implicits._
 
   // TODO pedro word 2 vect, (column vector concatenation ?)
+  val features = spark.read.parquet("features_extract.parquet").orderBy($"startYear")
+  features.createOrReplaceTempView("features")
 
+  features.show(50, false)
 
   // TODO pedro produce an X and Y dataset, output to csv
 }
